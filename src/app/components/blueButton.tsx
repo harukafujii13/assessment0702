f2883e396ba7622f9a2de281bc3c652c14cefdb5
Haruka Fujii <haruka@freelivenet.com>
2024-07-02 15:01:39 -0700

    🌟ADD: added onClick function

--- src/app/components/blueButton.tsx
+++ src/app/components/blueButton.tsx
@@ -5,12 +5,17 @@ import "../custom-bootstrap.scss";
 
 interface ButtonProps {
   title: string;
+  onClick?: () => void;
 }
 
-export function BlueButton({ title }: ButtonProps) {
+export function BlueButton({ title, onClick }: ButtonProps) {
   return (
-    <div className="d-flex justify-content-center">
-      <Button className="blue-button btn rounded-pill">{title}</Button>
+    <div className="d-flex justify-content-center w-100 mt-4">
+      <Button
+        className="blue-button btn rounded-pill w-100"
+        onClick={onClick}>
+        {title}
+      </Button>
     </div>
   );
 }
